43049537c71ac947f47e8145fbbf6b6b9133ef77
scrwdrv <scrwdrv.tech@gmail.com>
2020-01-09 22:51:01 +0800

    Auto commit @auto-committer
    1 file Modified: [src/index.ts](+1)(-1)

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ let watch = process.argv.indexOf('--watch') > -1 ? true : false,
     watchInterval = watch ? parseInt(process.argv[process.argv.indexOf('--watch') + 1]) || 60 : null,
     errors = 0;
 
-if (watch) console.log('\u001b[2J\u001b[0;0H');
+if (watch) console.log('\u001b[2J\u001b[0;0H\n');
 
 (function exec() {
 
